7a82201981ecf2bf7ff20719abe4a1eb4b86a579
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2025-02-28 01:34:45 +0530

    customize button

--- src/modules/home/ui/auth/ui/component/auth-button.tsx
+++ src/modules/home/ui/auth/ui/component/auth-button.tsx
@@ -3,7 +3,12 @@ import { UserCircleIcon } from "lucide-react"
 
 export const AuthButton = ()=>{
     return (
-        <Button >
+        <Button
+        variant={"outline"}
+        className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/2 rounded-full shadow-none
+        
+        "
+        >
               <UserCircleIcon/>
               Sign in
         </Button>

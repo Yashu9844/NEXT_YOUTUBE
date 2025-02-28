a7fa0856cc6b7576189fca0bc885bf218c5c3e0f
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2025-02-28 12:33:30 +0530

    add layout for auth pages

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+
+interface LayoutProps{
+    children?: React.ReactNode;
+}
+
+
+const Layout = ({children} : LayoutProps) => {
+  return (
+    <div className="min-h-screen  flex items-center justify-center bg-gray-100">
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
+

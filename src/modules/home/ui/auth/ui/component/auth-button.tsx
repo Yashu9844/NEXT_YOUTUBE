be62fff85ccc21052d7e57575e5f0f80f201ea50
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2025-02-28 12:44:13 +0530

    add authorization in ui

--- src/modules/home/ui/auth/ui/component/auth-button.tsx
+++ src/modules/home/ui/auth/ui/component/auth-button.tsx
@@ -1,8 +1,21 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
+import { SignedIn, UserButton } from "@clerk/clerk-react"
+import { SignedOut, SignInButton } from "@clerk/nextjs"
 import { UserCircleIcon } from "lucide-react"
 
 export const AuthButton = ()=>{
     return (
+       <>
+            <SignedIn>
+                <UserButton/>
+            </SignedIn>
+
+
+
+       <SignedOut>
+        <SignInButton mode="modal">
         <Button
         variant={"outline"}
         className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/2 rounded-full shadow-none
@@ -12,5 +25,9 @@ export const AuthButton = ()=>{
               <UserCircleIcon/>
               Sign in
         </Button>
+        </SignInButton>
+       </SignedOut>
+       
+       </>
     )
 }

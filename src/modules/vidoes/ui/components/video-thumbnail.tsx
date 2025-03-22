8dc31f2d7aea8db1ac5c542980d9a804b42bb30e
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2025-03-23 01:32:51 +0530

    small alt

--- src/modules/vidoes/ui/components/video-thumbnail.tsx
+++ src/modules/vidoes/ui/components/video-thumbnail.tsx
@@ -16,8 +16,8 @@ const VideoThumbnail = ({imageUrl,
     <div>
       <div className="relative group">
         <div className="relative aspect-video w-full rounded-xl overflow-hidden">
-            <Image src={imageUrl ?? '/placeholder.svg'} alt="TRhumbanil" fill className="h-full w-full object-cover group-hover:opacity-0 " />
-            <Image src={previewUrl ?? '/placeholder.svg'} alt="TRhumbanil" fill className="h-full w-full object-cover opacity-0 group-hover:opacity-100 " />
+            <Image src={imageUrl ?? '/placeholder.svg'} alt={title} fill className="h-full w-full object-cover group-hover:opacity-0 " />
+            <Image src={previewUrl ?? '/placeholder.svg'} alt={title} fill className="h-full w-full object-cover opacity-0 group-hover:opacity-100 " />
         </div>
       </div>
     </div>
